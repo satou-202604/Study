5cf56e67b952f6bae57cb59380f3442b5bd611b7
佐藤明日香 <273242775+satou-202604@users.noreply.github.com>
2026-04-19 20:08:16 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,30 @@
+public class Task2_6 {
+
+    public static void main(String[] args) {
+
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        int hogeInt = 50;
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogeString = "テスト";
+
+        // ③double型の変数hogeDoubleを宣言し、小数値0.8で初期化している
+        double hogeDouble = 0.8;
+
+        // ④変数 hogeInt を値「100」で上書き
+        hogeInt = 100;
+
+        // ⑤変数 hogeString を値「良くできました！」で上書き
+        hogeString = "良くできました！";
+
+        // ⑥変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+
+        // ⑦変数hogeStringの値（文字列）をコンソールに出力している
+        System.out.println(hogeString);
+
+        // ⑧変数 hogeDouble の値を表示
+        System.out.println(hogeDouble);
+
+    }
+}
