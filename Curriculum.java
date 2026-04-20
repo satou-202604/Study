4eb105788d85407efcc7f8e616ab7855c996bd22
佐藤明日香 <273242775+satou-202604@users.noreply.github.com>
2026-04-20 21:26:19 +0900

    2-7

---
+++ Curriculum.java
@@ -0,0 +1,7 @@
+public class Curriculum {
+  public static void main(String[] args) {
+    //int型を宣言して、「i」という変数名をつけて、1234で初期化する。
+    int i = 1234;
+    System.out.println("int型 = 整数 = " + i);
+  }
+}
